76ca90b6aabd12055bb30e6addc435c8c190c5cd
Saber FRAJ <frejsaber@gmail.com>
2021-03-07 11:37:25 +0100

    first commit

---
+++ src/main/java/com/automatemailing/AutomateMailingGmailDevApplication.java
@@ -0,0 +1,13 @@
+package com.automatemailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomateMailingGmailDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomateMailingGmailDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/automatemailing/AutomateMailingGmailDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automatemailing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomateMailingGmailDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
